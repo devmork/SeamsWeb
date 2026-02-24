87c39995ceb6c4e769c9f9ded8f5598b1a1a3b0b
devmork <enoymarkden@gmail.com>
2026-02-24 20:46:55 +0800

    feat: implement routing for login and registration pages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/Auth/LoginPage";
+import RegisterPage from "./pages/Auth/RegisterPage";
+import Layout from "./components/layouts/Layout";
 
 function App() {
   return (
     <>
-      <BrowserRouter></BrowserRouter>;
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          {/* nested protected pages go here, e.g. */}
+          {/* <Route index element={<Dashboard />} /> */}
+        </Route>
+         <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
