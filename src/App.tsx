dfdc0c82696e7c189f9bacced86cfbe25800dbfe
devmork <enoymarkden@gmail.com>
2026-02-26 22:09:22 +0800

    feat: update App component to use AuthenticatedLayout instead of Layout for routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/RegisterPage";
-import Layout from "./components/layouts/AuthenticatedLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AuthenticatedLayout from "./components/layouts/AuthenticatedLayout";
 
 function App() {
   return (
@@ -14,17 +14,9 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
 
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout />
-            </ProtectedRoute>
-          }
-        >
-        </Route>
-        
-         <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<ProtectedRoute><AuthenticatedLayout role={"admin"} /></ProtectedRoute>} />
+
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </>
