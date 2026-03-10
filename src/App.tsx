b6f52448644e7eec71a22422af8fbce8b4486245
devmork <enoymarkden@gmail.com>
2026-03-10 23:22:53 +0800

    feat: remove ProtectedRoute and AuthenticatedLayout from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/RegisterPage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import AuthenticatedLayout from "./components/layouts/AuthenticatedLayout";
 
 function App() {
   return (
@@ -14,7 +12,6 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
 
-          <Route path="/" element={<ProtectedRoute><AuthenticatedLayout role={"admin"} /></ProtectedRoute>} />
 
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
