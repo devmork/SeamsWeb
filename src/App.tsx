3f07aee1b33faee91924257cd764838fc720f32c
Jibonnnnn. <jeevonricafort@gmail.com>
2026-03-16 14:43:59 +0800

    feat: integrate StudentDashboard component and enhance UI with new card and badge components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./pages/Auth/LoginPage";
-import RegisterPage from "./pages/Auth/RegisterPage";
+import LoginPage from "./Pages/Auth/LoginPage";
+import RegisterPage from "./Pages/Auth/RegisterPage";
+import { StudentDashboard } from "./Pages/StudentDashboard";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+
+          <Route path="/student" element={<StudentDashboard onLogout={() => {}} isApproved={true} />} />
 
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
