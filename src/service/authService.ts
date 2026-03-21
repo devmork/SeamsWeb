5f58aaff6b701e9e44c6bc5cb0219c4e0258eded
GiroGodito <goditolitoarthon@gmail.com>
2026-03-21 11:57:43 +0800

    enhancement: integrated profile pic upload for students, added fileUploadService.ts handling file uploads

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "https://localhost:7122/api";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
