9f3bc0b0ab807467dda5fec70d6bf352439f35dd
devmork <enoymarkden@gmail.com>
2026-06-14 01:20:18 +0800

    feat: register the log in and sign up form routes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./Pages/Auth/LoginPage";
-import RegisterPage from "./Pages/Auth/RegisterPage";
-import QRScanner from "./Pages/Officer/QRScanner";
-import { StudentDashboard } from "./Pages/StudentDashboard";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import QRScanner from "./pages/Officer/QRScanner";
+import { StudentDashboard } from "./pages/StudentDashboard";
+import { LoginForm } from "./pages/Auth/LoginForm";
+import { SignupForm } from "./pages/Auth/SignupForm";
+import RegisterPage from "./pages/Auth/RegisterPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/officer" element={<QRScanner />} />
-          <Route path="/student" element={<StudentDashboard onLogout={function (): void {
-            throw new Error("Function not implemented.");
-          } } isApproved={false} />} />
+          <Route
+            path="/student"
+            element={
+              <StudentDashboard
+                onLogout={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                isApproved={false}
+              />
+            }
+          />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
