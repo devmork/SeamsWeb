6635536befe8b34f71f86d8890c4a90d2f68e2c3
devmork <enoymarkden@gmail.com>
2026-09-08 16:18:20 +0800

    feat: update import path for routeTree in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/query-client';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { routeTree } from './tanstack-routeTree.gen.ts';
+import { routeTree } from './routeTree.gen';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
 const router = createRouter({ routeTree });
