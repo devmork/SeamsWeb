d3f4c1b92f0c567255b88fe9c8548370b2d61984
Rafflesia00 <cardaluu6@gmail.com>
2026-09-12 00:28:44 +0800

    updated the loginform signup word to create an account

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -162,7 +162,7 @@ export default function LoginForm() {
             <FieldDescription className="text-center">
               Don&apos;t have an account?{' '}
               <Link to="/signup" className="underline underline-offset-4">
-                Sign up
+                Create an account
               </Link>
             </FieldDescription>
           </Field>
